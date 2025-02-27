293940fd1dc613d0506c4d76b61cf00a5b3c4be4
alexljn5 <alexljn5@gmail.com>
2025-02-27 09:43:15 +0100

    smol fix

--- idletest/5secondreaper.js
+++ idletest/5secondreaper.js
@@ -1,30 +1,28 @@
 import { state } from './main.js';
 
-
 let numberOfFiveSecondReaper = 0;
 let costOfFiveSecondreaper = 15;
+
 document.getElementById("fiveSecondReaper").addEventListener("click", fiveSecondReaper, false);
+
+document.getElementById("countOfFiveSecondReapers").value = numberOfFiveSecondReaper;
+document.getElementById("costOfFiveSecondReapers").value = costOfFiveSecondreaper;
 
 function fiveSecondReaper() {
     if (costOfFiveSecondreaper <= Math.round(state.clicks)) {
-        state.clicks = state.clicks - costOfFiveSecondreaper;
+        state.clicks -= costOfFiveSecondreaper;
+
         setInterval(() => {
             state.clicks++;
-            document.getElementById("counter").value;
             document.getElementById("counter").value = Math.round(state.clicks);
-            console.log(state.clicks);
-        }, 5000); //For future me, value needs to be in milliseconds
+        }, 5000);
+
         document.getElementById("counter").value = Math.round(state.clicks);
 
-
-        document.getElementById("countOfFiveSecondReapers").value;
         numberOfFiveSecondReaper++;
         document.getElementById("countOfFiveSecondReapers").value = numberOfFiveSecondReaper;
-        costOfFiveSecondreaper = costOfFiveSecondreaper + costOfFiveSecondreaper * 0.5;
 
+        costOfFiveSecondreaper += costOfFiveSecondreaper * 0.5;
         document.getElementById("costOfFiveSecondReapers").value = Math.round(costOfFiveSecondreaper);
-    } else {
-
     }
-
 }
--- idletest/inventory.js
+++ idletest/inventory.js
@@ -2,37 +2,45 @@ import { state } from './main.js';
 
 let inventory = [];
 
-let giftFromTheGods = "giftfromthegods";
-let doubleTrouble = "doubletrouble";
+let giftFromTheGodsItem = "giftfromthegods";
+let doubleTroubleItem = "doubletrouble";
 
-document.getElementById("inventory").addEventListener("click", inventoryArray, false);
+document.getElementById("giftfromthegods").addEventListener("click", giftFromTheGods, false);
+document.getElementById("doubletrouble").addEventListener("click", doubleTrouble, false);
 
-function inventoryArray() {
-    if (state.clicks >= 5 && !inventory.includes(giftFromTheGods)) {
-        inventory.push(giftFromTheGods);
+function giftFromTheGods() {
+    if (state.clicks >= 5 && !inventory.includes(giftFromTheGodsItem)) {
+        inventory.push(giftFromTheGodsItem);
         state.clicks = state.clicks + 500;
         document.getElementById("giftfromthegods").disabled = true;
         document.getElementById("counter").value = Math.round(state.clicks); //So counter displays accurate
+    } else {
+        alert("not enough clicks");
     }
 
+    document.getElementById("counter").value = Math.round(state.clicks);
+    console.log(inventory);
+}
 
-    /*
-    This somehow doesnt work, need fixing
+function doubleTrouble() {
     if (state.clicks >= 10 && !inventory.includes(doubleTrouble)) {
         inventory.push(doubleTrouble);
         state.clicks = state.clicks * 2;
         document.getElementById("doubletrouble").disabled = true;
-
+        document.getElementById("counter").value = Math.round(state.clicks);
     } else {
-        //alert("not enough clicks");
+        alert("not enough clicks");
     }
-*/
-    document.getElementById("counter").value = Math.round(state.clicks);
-    console.log(inventory);
+}
 
-    /*if (document.getElementById("giftfromthegods").disabled = true && (document.getElementById("doubletrouble").disabled = true) && (state.clicks = 10)) {
-        //alert("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }*/
+/* Eventual code to loop through array to find existing strings so you cant exploit the game... i think
+
+
+for(let i = 0; i < inventory.length; i++) {
+
 }
 
 
+
+
+*/
